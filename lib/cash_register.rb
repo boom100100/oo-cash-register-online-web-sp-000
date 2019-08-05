0c7d456dfaa4bb4e2faf9b07780afb7fab1e385e
Bernadette Davis <bsd231@nyu.edu>
2019-08-05 14:18:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ class CashRegister
   end
 
   def apply_discount
-    @total = (@total - (@total * (@discount / 100)))
+    @total = @total - (@total * (@discount / 100))
     puts @total
   end
 end
