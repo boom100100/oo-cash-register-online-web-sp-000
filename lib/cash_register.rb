6cea32a8d57e228f798a3438abefe4d545c73095
Bernadette Davis <bsd231@nyu.edu>
2019-08-05 14:36:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,9 +11,7 @@ class CashRegister
   end
 
   def total
-    #@total = (@total - (@total * (@discount / 100)))
     @total
-    #puts @total
   end
 
   def total=(arg)
@@ -23,6 +21,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     self.total=(price * quantity)
   end
+
   def apply_discount
     if discount == 0
       "There is no discount to apply."
@@ -31,4 +30,9 @@ class CashRegister
       "After the discount, the total comes to $#{@total.to_i}."
     end
   end
+
+  def items
+    return @items
+  end
+
 end
