f22f22831d3d3b1d24bd141c29da55d1ae008b50
Bernadette Davis <bsd231@nyu.edu>
2019-08-05 14:09:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
 
+  def initialize(disc = 0)
+    @total = 0
+    @discount = disc
+  end
+
+  def discount
+    @discount
+  end
+  def total
+    apply_discount
+    @total
+  end
+  def total=(arg)
+    @total = @total + arg
+  end
+  def add_item(title, price, quantity = 1)
+    self.total=(price * quantity)
+
+  end
+  def apply_discount
+    @total = (@total - (@total * (@discount / 100)))
+  end
+end
