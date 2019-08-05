7b9279bb21eb14f57e5ccaf13aaa82214c698a52
Bernadette Davis <bsd231@nyu.edu>
2019-08-05 14:43:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    for qualtity.times |int| do
+    for qualtity.times do
       @items << title
     end
     self.total=(price * quantity)
